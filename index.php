3aa201c02006f63852cfee167259fa5b2898beb5
jazz2canvey <jazz2canvey@gmail.com>
2019-07-13 18:00:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,6 +23,8 @@ require_once 'libraries/common.inc.php';
  */
 require_once 'libraries/display_git_revision.lib.php';
 
+echo $_SERVER['DOCUMENT_ROOT'];
+
 /**
  * pass variables to child pages
  */
